190d3b638c99535fe192cd269741b1c1e8b3ba18
Samiul Muztaba <samiulmuztaba@outlook.com>
2023-10-11 23:02:27 +0600

    first commit

---
+++ 06 - Getters and Setters/main.py
@@ -0,0 +1,7 @@
+from item import Item
+from phone import Phone
+
+item1 = Item("Samsung", 100000, 1)
+
+item1.name = "Samsung"
+print(item1.name)
---
+++ 07 - OOP Principles/main.py
@@ -0,0 +1,7 @@
+from phone import Phone
+from keyboard import Keyboard
+
+item1 = Keyboard("Samsung", 100)
+
+item1.apply_discount()
+print(item1.price)
